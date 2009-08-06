2baa8c4c9f6896b71290b4189f6e17d88dab1ebe
lijin.unix <lijin.unix@13de9a6c-71d3-11de-b374-81e7cb8b6ca2>
2009-08-06 14:31:05 +0000

    问题多多

--- NXP-LPC/V6/Bsp/LPC2378/app_cfg.h
+++ NXP-LPC/V6/Bsp/LPC2378/app_cfg.h
@@ -45,10 +45,7 @@
 */
 
 #define  APP_TASK_START_PRIO                   1
-#define  APP_TASK_KBD_PRIO                     2
-#define  OS_VIEW_TASK_PRIO                     3
-#define  OS_VIEW_TASK_ID                       3
-#define  APP_TASK_SER_PRIO                     4
+
 #define  APP_TASK_PHY_PRIO                     5
 #define  NET_OS_CFG_IF_RX_TASK_PRIO            6
 #define  NET_OS_CFG_TMR_TASK_PRIO              7
@@ -60,9 +57,7 @@
 *********************************************************************************************************
 */
 
-#define  APP_TASK_START_STK_SIZE             128
-#define  APP_TASK_KBD_STK_SIZE               128 
-#define  APP_TASK_SER_STK_SIZE               256
+#define  APP_TASK_START_STK_SIZE             256
 #define  APP_TASK_PHY_STK_SIZE               128
 #define  NET_OS_CFG_TMR_TASK_STK_SIZE       1024
 #define  NET_OS_CFG_IF_RX_TASK_STK_SIZE      512
@@ -74,23 +69,6 @@
 *********************************************************************************************************
 */
 
-#define  OS_VIEW_PARSE_TASK                    0                /* Parsing of received packets will be done by a task       */
-#define  OS_VIEW_TMR_32_BITS                   1                /* uC/OS-View timer is 32 bits                              */
-
-#define  OS_VIEW_UART_0                        0                /* UART0 selected                                           */
-#define  OS_VIEW_UART_1                        1                /* UART1 selected                                           */
-
-#define  OS_VIEW_COMM_SEL         OS_VIEW_UART_0                /* Select UART0 for uC/OS-View to use                       */
-
-/*
-*********************************************************************************************************
-*                                           RS-232 CONSTANTS
-*********************************************************************************************************
-*/
-
-#define  SER_UART_0                            0                /* UART0 selected                                           */
-#define  SER_UART_1                            1                /* UART1 selected                                           */
-#define  SER_COMM_SEL                 SER_UART_1                /* Select UART1 for printing                                */
 
 /*
 *********************************************************************************************************
