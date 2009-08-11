302d31020fc8da3fb5c00fc2b18f747e9eda97c0
lijin.unix <lijin.unix@13de9a6c-71d3-11de-b374-81e7cb8b6ca2>
2009-08-11 08:35:54 +0000

    中断收到一次数据后，无法再收到。

--- trunk/NXP-LPC/RT-Thread/bsp/lpc2378/emac.h
+++ trunk/NXP-LPC/RT-Thread/bsp/lpc2378/emac.h
@@ -10,7 +10,7 @@ rt_uint8_t write_phy (rt_uint32_t phyadd,rt_uint32_t PhyReg, rt_uint32_t Value);
 rt_uint16_t read_phy_ex ( rt_uint16_t phyadd ,rt_uint8_t  PhyReg,rt_uint16_t *err);
 void  write_phy_ex (rt_uint8_t  phy, rt_uint8_t  reg, rt_uint16_t  val, rt_uint16_t  *perr);
 
-rt_err_t rt_emac_init(rt_device_t dev);
+rt_err_t lpc24xxether_init(rt_device_t dev);
 
 void  nic_link_change(rt_uint32_t link_speed, rt_uint32_t link_duplex);
 
