9bf21d8e6dcf1793b3af6c3bb0eb7a5fa586a0dd
lijin.unix <lijin.unix@13de9a6c-71d3-11de-b374-81e7cb8b6ca2>
2009-08-17 01:45:10 +0000

    发送部分驱动还有点问题。待查。

--- trunk/NXP-LPC/RT-Thread/bsp/lpc2378/application.c
+++ trunk/NXP-LPC/RT-Thread/bsp/lpc2378/application.c
@@ -18,13 +18,23 @@
  * @addtogroup LPC2478
  */
 /*@{*/
-
+ 
 char app_thread_stack[512];
 struct rt_thread app_thread;
+extern void app_thread_entry(void* parameter);
 
 int rt_application_init()
 { 
-	extern void app_thread_entry(void* parameter);
+	/* LwIP Initialization */
+#ifdef RT_USING_LWIP
+	{
+		extern void lwip_sys_init(void);
+
+		/* init lwip system */
+		lwip_sys_init();
+		rt_kprintf("TCP/IP initialized!\n");
+	}
+#endif 
 	rt_thread_init(&app_thread,"app_thread",app_thread_entry, RT_NULL,	&app_thread_stack[0], sizeof(app_thread_stack),
 		20, 10);
 
