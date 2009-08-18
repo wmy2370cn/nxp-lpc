7e43190a9aa5085ff5ffa27eedcfb55e0fe57c18
lijin.unix <lijin.unix@13de9a6c-71d3-11de-b374-81e7cb8b6ca2>
2009-08-18 09:03:53 +0000

    准备开始移植UCOS2.88+lwip1.3

---
+++ NXP-LPC/V7/Bsp/LPC2378/app.c
@@ -0,0 +1,193 @@
+/*
+*********************************************************************************************************
+*                                              EXAMPLE CODE
+*
+*                          (c) Copyright 2003-2006; Micrium, Inc.; Weston, FL
+*
+*               All rights reserved.  Protected by international copyright laws.
+*               Knowledge of the source code may NOT be used to develop a similar product.
+*               Please help us continue to provide the Embedded community with the finest
+*               software available.  Your honesty is greatly appreciated.
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                             EXAMPLE CODE
+*
+*                                             NXP LPC2468
+*                                                on the
+*                                  Embedded Artists LPC2468 OEM Board
+*
+* Filename      : app.c
+* Version       : V1.00
+* Programmer(s) : Brian Nagel
+*********************************************************************************************************
+*/
+
+#include <includes.h>
+
+#if uC_TCPIP_MODULE > 0
+#include <net_phy.h>
+#include <net_phy_def.h>
+#endif
+
+/*
+*********************************************************************************************************
+*                                           #DEFINE CONSTANTS
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                            LOCAL VARIABLES
+*********************************************************************************************************
+*/
+
+static  OS_STK          AppTaskStartStk[APP_TASK_START_STK_SIZE];
+
+#if uC_TCPIP_MODULE > 0
+        NET_IP_ADDR     AppNetIP;
+        NET_IP_ADDR     AppNetMsk;
+        NET_IP_ADDR     AppNetGateway;
+#endif
+
+/*
+*********************************************************************************************************
+*                                         LOCAL FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+static  void            AppTaskStart                (void *p_arg); 
+
+#if uC_TCPIP_MODULE > 0
+static  void            AppInitTCPIP                (void);
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                                main()
+*
+* Description : This is the standard entry point for C code.  It is assumed that your code will call
+*               main() once you have performed all necessary 68HC12 and C initialization.
+*
+* Arguments   : none
+*
+* Returns     : none
+*********************************************************************************************************
+*/
+
+int  main (void)
+{
+    CPU_INT08U  err;
+
+
+    BSP_IntDisAll();                                            /* Disable all interrupts until we are ready to accept them */
+
+    OSInit();                                                   /* Initialize "uC/OS-II, The Real-Time Kernel"              */
+
+    OSTaskCreateExt(AppTaskStart,                               /* Create the start task                                    */
+                    (void *)0,
+                    (OS_STK *)&AppTaskStartStk[APP_TASK_START_STK_SIZE - 1],
+                    APP_TASK_START_PRIO,
+                    APP_TASK_START_PRIO,
+                    (OS_STK *)&AppTaskStartStk[0],
+                    APP_TASK_START_STK_SIZE,
+                    (void *)0,
+                    OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
+
+#if OS_TASK_NAME_SIZE > 13
+    OSTaskNameSet(APP_TASK_START_PRIO, "Start Task", &err);
+#endif
+
+    OSStart();                                                  /* Start multitasking (i.e. give control to uC/OS-II)       */
+}
+
+/*
+*********************************************************************************************************
+*                                          STARTUP TASK
+*
+* Description : This is an example of a startup task.  As mentioned in the book's text, you MUST
+*               initialize the ticker only once multitasking has started.
+*
+* Arguments   : p_arg   is the argument passed to 'AppStartTask()' by 'OSTaskCreate()'.
+*
+* Returns     : none
+*
+* Notes       : 1) The first line of code is used to prevent a compiler warning because 'p_arg' is not
+*                  used.  The compiler should not generate any code for this statement.
+*********************************************************************************************************
+*/
+
+static  void  AppTaskStart (void *p_arg)
+{
+    void           *msg;
+    CPU_INT08U      err;
+    
+
+    (void)p_arg;
+
+    BSP_Init();                                                 /* Initialize BSP functions                                 */
+
+#if OS_TASK_STAT_EN > 0
+    OSStatInit();                                               /* Determine CPU capacity                                   */
+#endif
+  
+#if uC_TCPIP_MODULE > 0
+    AppInitTCPIP();                                             /* Initialize uC/TCP-IP and associated appliations          */
+#endif
+
+  
+    
+	while (DEF_TRUE)
+	{                         
+	//	OSTimeDlyHMSM(0, 0, 1, 0);
+		OSTimeDlyHMSM(0,0,1,0);
+		LED_On(1);
+		OSTimeDlyHMSM(0,0,1,0);
+	 	LED_Off(1);
+	}
+}
+
+/*
+*********************************************************************************************************
+*                                      AppInitTCPIP()
+*
+* Description : This function is called by AppTaskStart() and is responsible for initializing uC/TCP-IP
+*               uC/HTTPs, uC/TFTPs and uC/DHCPc if enabled.
+*
+* Arguments   : none
+*
+* Returns     : none
+*********************************************************************************************************
+*/
+
+#if uC_TCPIP_MODULE > 0
+static  void  AppInitTCPIP (void)
+{
+    NET_ERR  err;
+
+
+#if EMAC_CFG_MAC_ADDR_SEL == EMAC_CFG_MAC_ADDR_SEL_CFG
+    NetIF_MAC_Addr[0] = 0x00;
+    NetIF_MAC_Addr[1] = 0x38;
+    NetIF_MAC_Addr[2] = 0x6c;
+    NetIF_MAC_Addr[3] = 0xa2;
+    NetIF_MAC_Addr[4] = 0x45;
+    NetIF_MAC_Addr[5] = 0x5e;
+#endif
+
+    err             = Net_Init();                               /* Initialize uC/TCP-IP                                     */
+
+    AppNetIP        = NetASCII_Str_to_IP("192.9.200.128",  &err);
+    AppNetMsk       = NetASCII_Str_to_IP("255.255.255.0", &err);
+    AppNetGateway   = NetASCII_Str_to_IP("192.9.200.1",   &err);
+
+    err             = NetIP_CfgAddrThisHost(AppNetIP, AppNetMsk);
+    err             = NetIP_CfgAddrDfltGateway(AppNetGateway);
+}
+#endif 
+
+  
---
+++ NXP-LPC/V7/Bsp/LPC2468/bsp_i2c.c
@@ -0,0 +1,397 @@
+/*
+*********************************************************************************************************
+*                                     MICRIUM BOARD SUPPORT SUPPORT
+*
+*                          (c) Copyright 2003-2006; Micrium, Inc.; Weston, FL
+*
+*               All rights reserved.  Protected by international copyright laws.
+*               Knowledge of the source code may NOT be used to develop a similar product.
+*               Please help us continue to provide the Embedded community with the finest
+*               software available.  Your honesty is greatly appreciated.
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                           I2C FUNCTIONS
+*
+*                                             NXP LPC2468
+*                                                on the
+*                                  Embedded Artists LPC2468 OEM Board
+*
+* Filename      : bsp.c
+* Version       : V1.00
+* Programmer(s) : Brian Nagel
+* Note(s)       : (1) Adapted from uCLinux distribution.
+*********************************************************************************************************
+*/
+
+#define  BSP_GLOBALS
+#include <includes.h>
+#include <LPC24xx.H>
+
+/*
+*********************************************************************************************************
+*                                          #DEFINE CONSTANTS
+*********************************************************************************************************
+*/
+
+#define  I2CON_AA                   DEF_BIT_02
+#define  I2CON_SI                   DEF_BIT_03
+#define  I2CON_STO                  DEF_BIT_04
+#define  I2CON_STA                  DEF_BIT_05
+#define  I2CON_I2EN                 DEF_BIT_06
+
+                                                                /* ---- I2C STATUS CODES: MASTER IN TRANSMITTER MODE ------ */
+#define  I2STAT_SLA_W_ACK               0x18
+#define  I2STAT_SLA_W_NOACK             0x20
+#define  I2STAT_DAT_T_ACK               0x28
+#define  I2STAT_DAT_T_NOACK             0x30
+
+                                                                /* ------ I2C STATUS CODES: MASTER IN RECEIVER MODE ------- */
+#define  I2STAT_SLA_R_ACK               0x40
+#define  I2STAT_SLA_R_NOACK             0x48
+#define  I2STAT_DAT_R_ACK               0x50
+#define  I2STAT_DAT_R_NOACK             0x58
+
+                                                                /* ---------- I2C STATUS CODES: MASTER AND SLAVE ---------- */
+#define  I2STAT_START                   0x08
+#define  I2STAT_REPEATED_START          0x10
+#define  I2STAT_ARB_LOST                0x38
+#define  I2STAT_NOP                     0x58
+#define  I2STAT_BUS_ERROR               0x00
+
+
+#define  PCA9532_ADDRESS                0xC0
+
+#define  PCA9532_INPUT0                 0x00
+#define  PCA9532_INPUT1                 0x01
+#define  PCA9532_PSC0                   0x02
+#define  PCA9532_PWM0                   0x03
+#define  PCA9532_PSC1                   0x04
+#define  PCA9532_PWM1                   0x05
+#define  PCA9532_LS0                    0x06
+#define  PCA9532_LS1                    0x07
+#define  PCA9532_LS2                    0x08
+#define  PCA9532_LS3                    0x09
+
+/*
+*********************************************************************************************************
+*                                               DATA TYPES
+*********************************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************************
+*                                              VARIABLES
+*********************************************************************************************************
+*/
+
+static  CPU_INT08U      I2C_Address;
+static  CPU_INT08U     *I2C_BufPtr;
+static  CPU_INT08U      I2C_Count;
+
+static  OS_EVENT       *I2C_SemWait;
+static  OS_EVENT       *I2C_SemBusy;
+
+/*
+*********************************************************************************************************
+*                                              PROTOTYPES
+*********************************************************************************************************
+*/
+
+
+/*
+******************************************************************************************************************************
+******************************************************************************************************************************
+**                                         Global Functions
+******************************************************************************************************************************
+******************************************************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************************
+*                                        I2C_Init()
+*
+* Description : This function initializes the I2C
+*
+* Arguements  : None
+*
+* Returns     : None
+*********************************************************************************************************
+*/
+
+void  I2C_Init (void)
+{
+    CPU_INT32U  pinsel;
+
+
+    VICIntSelect       &= ~(1 << VIC_I2C0);                     /* Configure the I2C interrupt as an IRQ source             */
+    VICVectAddr9        =  (CPU_INT32U)I2C_ISR_Handler;         /* Set the vector address                                   */
+    VICIntEnClr       =  (1 << VIC_I2C0);                     /* Disable the interrupt source                             */
+
+    pinsel              = PINSEL1;
+    pinsel             &= 0xFC3FFFFF;
+    pinsel             |= 0x01400000;
+    PINSEL1             = pinsel;
+
+    I20CONCLR           = 0x006C;                               /* Clear flags                                              */
+
+    I20SCLH             = 0x00F0;
+    I20SCLL             = 0x0160;
+
+    I2C_SemWait         = OSSemCreate(0);
+    I2C_SemBusy         = OSSemCreate(1);
+}
+
+/*
+*********************************************************************************************************
+*                                        I2C_Read()
+*
+* Description : This function reads from the I2C bus.
+*
+* Arguements  : p_buf   is a pointer to the buffer into which the bytes will be stored
+*               count   is the number of bytes to read
+*
+* Returns     : None
+*********************************************************************************************************
+*/
+
+void  I2C_Read (CPU_INT08U *p_buf, CPU_INT08U count)
+{
+    CPU_INT08U      err;
+
+
+    OSSemPend(I2C_SemBusy, 0, &err);                            /* Wait until I2C is free                                   */
+
+    I20CONCLR           = I2CON_I2EN
+                        | I2CON_STA
+                        | I2CON_SI
+                        | I2CON_AA;
+
+    I20CONSET           = I2CON_I2EN;
+
+    I2C_Address         = (PCA9532_ADDRESS | 0x01);             /* Set lower bit of address                                 */
+    I2C_BufPtr          = p_buf;
+    I2C_Count           = count;
+
+    I20CONSET           = I2CON_STA
+                        | I2CON_AA;
+
+
+    VICIntEnable        =  (1 << VIC_I2C0);                     /* Enable the interrupt source                              */
+    OSSemPend(I2C_SemWait, 0, &err);                            /* Wait until complete                                      */
+
+    OSSemPost(I2C_SemBusy);                                     /* Free I2C                                                 */
+}
+
+/*
+*********************************************************************************************************
+*                                        I2C_ReadReg()
+*
+* Description : This function reads a register on an I2C device.
+*
+* Arguements  : p_buf   is a pointer to the buffer into which the bytes will be stored
+*               count   is the number of bytes to read
+*               reg     is the register to read
+*
+* Returns     : None
+*********************************************************************************************************
+*/
+
+void  I2C_ReadReg (CPU_INT08U *p_buf, CPU_INT08U count, CPU_INT08U reg)
+{
+    CPU_INT08U      err;
+
+
+    OSSemPend(I2C_SemBusy, 0, &err);                            /* Wait until I2C is free                                   */
+
+
+
+    I20CONCLR           = I2CON_I2EN
+                        | I2CON_STA
+                        | I2CON_SI
+                        | I2CON_AA;
+
+    I20CONSET           = I2CON_I2EN;
+
+    I2C_Address         = (PCA9532_ADDRESS & 0xFE);             /* Clear lower bit of address                               */
+    I2C_BufPtr          = &reg;
+    I2C_Count           = 1;
+
+    I20CONSET           = I2CON_STA
+                        | I2CON_AA;
+
+    VICIntEnable        =  (1 << VIC_I2C0);                     /* Enable the interrupt source                              */
+    OSSemPend(I2C_SemWait, 0, &err);                            /* Wait until complete                                      */
+
+
+
+    I20CONCLR           = I2CON_I2EN
+                        | I2CON_STA
+                        | I2CON_SI
+                        | I2CON_AA;
+
+    I20CONSET           = I2CON_I2EN;
+
+    I2C_Address         = (PCA9532_ADDRESS | 0x01);             /* Set lower bit of address                                 */
+    I2C_BufPtr          = p_buf;
+    I2C_Count           = count;
+
+    I20CONSET           = I2CON_STA
+                        | I2CON_AA;
+
+
+    VICIntEnable        =  (1 << VIC_I2C0);                     /* Enable the interrupt source                              */
+    OSSemPend(I2C_SemWait, 0, &err);                            /* Wait until complete                                      */
+
+
+    OSSemPost(I2C_SemBusy);                                     /* Free I2C                                                 */
+}
+
+/*
+*********************************************************************************************************
+*                                        I2C_Write()
+*
+* Description : This function writes to the I2C bus.
+*
+* Arguements  : p_buf   is a pointer to the buffer storing the data to be written
+*               count   is the number of bytes to write
+*
+* Returns     : None
+*********************************************************************************************************
+*/
+
+void  I2C_Write (CPU_INT08U *p_buf, CPU_INT08U count)
+{
+    CPU_INT08U      err;
+
+
+    OSSemPend(I2C_SemBusy, 0, &err);                            /* Wait until I2C is free                                   */
+
+    I20CONCLR           = I2CON_I2EN
+                        | I2CON_STA
+                        | I2CON_SI
+                        | I2CON_AA;
+
+    I20CONSET           = I2CON_I2EN;
+
+    I2C_Address         = (PCA9532_ADDRESS & 0xFE);             /* Clear lower bit of address                               */
+    I2C_BufPtr          = p_buf;
+    I2C_Count           = count;
+
+    I20CONSET           = I2CON_STA
+                        | I2CON_AA;
+
+    VICIntEnable        =  (1 << VIC_I2C0);                     /* Enable the interrupt source                              */
+    OSSemPend(I2C_SemWait, 0, &err);                            /* Wait until complete                                      */
+
+    OSSemPost(I2C_SemBusy);                                     /* Free I2C                                                 */
+}
+
+/*
+*********************************************************************************************************
+*                                        I2C_ISR_Handler()
+*
+* Description : This function is the I2C ISR handler.
+*
+* Arguements  : None
+*
+* Returns     : None
+*********************************************************************************************************
+*/
+
+void  I2C_ISR_Handler (void)
+{
+    CPU_INT32U  status;
+    CPU_INT32U  temp;
+
+
+    status      = I20STAT;
+    temp        = 0;
+
+    switch (status & 0xF8) {
+
+        case I2STAT_START:
+        case I2STAT_REPEATED_START:
+             I20DAT             = I2C_Address;
+             I20CONCLR          = I2CON_STA | I2CON_SI;
+             break;
+
+        case I2STAT_SLA_W_ACK:
+             temp               = *I2C_BufPtr;
+             I20DAT             = temp;
+             I2C_BufPtr        += 1;
+             I2C_Count         -= 1;
+             I20CONCLR          = I2CON_STA | I2CON_SI;
+             break;
+
+        case I2STAT_DAT_T_ACK:
+             if (I2C_Count > 0) {
+                 temp           = *I2C_BufPtr;
+                 I20DAT         = temp;
+                 I2C_BufPtr    += 1;
+                 I2C_Count     -= 1;
+                 I20CONCLR      = I2CON_STA | I2CON_SI;
+             } else {
+                 I20CONCLR      = I2CON_STA | I2CON_SI;
+                 I20CONSET      = I2CON_STO;
+                 OSSemPost(I2C_SemWait);
+                 VICIntEnClr  =  (1 << VIC_I2C0);
+             }
+             break;
+
+        case I2STAT_SLA_W_NOACK:
+        case I2STAT_DAT_T_NOACK:
+        case I2STAT_SLA_R_NOACK:
+             I20CONSET          = I2CON_STO;
+             I20CONCLR          = I2CON_STA | I2CON_SI;
+             OSSemPost(I2C_SemWait);
+             VICIntEnClr      =  (1 << VIC_I2C0);
+             break;
+
+        case I2STAT_ARB_LOST:
+             I20CONCLR          = I2CON_STA | I2CON_SI;
+             OSSemPost(I2C_SemWait);
+             VICIntEnClr      =  (1 << VIC_I2C0);
+             break;
+
+
+        case I2STAT_SLA_R_ACK:
+             if (I2C_Count <= 1) {
+                 I20CONSET      = I2CON_AA;
+             } else {
+                 I20CONCLR      = I2CON_AA;
+             }
+             I20CONCLR          = I2CON_STA | I2CON_SI;
+             break;
+
+        case I2STAT_DAT_R_ACK:
+             temp               = I20DAT;
+            *I2C_BufPtr         = temp;
+             I2C_BufPtr        += 1;
+             I2C_Count         -= 1;
+             if (I2C_Count <= 1) {
+                 I20CONCLR      = I2CON_AA;
+             }
+             I20CONCLR          = I2CON_STA | I2CON_SI;
+             break;
+
+        case I2STAT_DAT_R_NOACK:
+             temp               = I20DAT;
+            *I2C_BufPtr         = temp;
+             I2C_Count         -= 1;
+             I20CONSET          = I2CON_STO;
+             I20CONCLR          = I2CON_STA | I2CON_SI;
+             OSSemPost(I2C_SemWait);
+             VICIntEnClr      =  (1 << VIC_I2C0);
+             break;
+
+        default:
+             I20CONCLR          = I2CON_STA | I2CON_SI;
+             break;
+    }
+}
