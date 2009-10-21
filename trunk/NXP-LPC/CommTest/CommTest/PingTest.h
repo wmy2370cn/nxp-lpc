f5007e1304aa4cb56948503acaef08b5e62b32ae
lijin.unix <lijin.unix@13de9a6c-71d3-11de-b374-81e7cb8b6ca2>
2009-10-21 03:38:34 +0000

    采用RAW SOCKET 模式重新PING部分。

--- trunk/NXP-LPC/CommTest/CommTest/PingTest.h
+++ trunk/NXP-LPC/CommTest/CommTest/PingTest.h
@@ -1,16 +1,25 @@
 
 #pragma  once 
 
-#include <vector>
+#include "ipstructs.h"
 
 class CPingTestDoc;
 
-class CPingTest
+struct CPingReply
 {
-public:
-	CPingTest();
-	~CPingTest( );
+	in_addr	 Address;		//The IP address of the replier
+	unsigned long avgRTT;	//Round Trip time in Milliseconds
+	unsigned long maxRTT;	
+	unsigned long minRTT;
+	int nError;
+};
 
+class CPingTask
+{
+	friend UINT  PingTestTask (LPVOID lpParam);	
+public:
+	CPingTask( );
+	~CPingTask( );
 public:
 	//开始收发
 	void StartTask( );
@@ -18,18 +27,36 @@ public:
 	void StopTask( );
 
 	void Engine( );
-
-	CPingTestDoc *m_pDocument;
-
-	std::vector <CWinThread *> m_arrTask;
-
-	DWORD  m_dwDestIp;
-	unsigned int m_nDestPortNum;
-	unsigned int m_nLocalPortNum;
- 
+private:
+	CWinThread *m_pPingTsk;
 	HANDLE      m_hStopEvent;
-	HANDLE m_hTaskHandle;
- 
-};
+	HANDLE      m_hTaskHandle;
 
+	DWORD       m_nThreadID;
+	unsigned short m_nSeq;
+
+	unsigned char m_nTTL;
+	unsigned char m_nTOS;
+	BOOL 	m_bDontFragment;
+	SOCKET  m_hSocketRaw;
+
+	unsigned int m_nPacketSize;
+	DWORD  m_dwDestIp;
+	DWORD  m_dwTimeout;
+
+	CPingReply  m_PingReply;
+
+	unsigned char  *m_pSendBuf;
+	unsigned char  *m_pRecvBuf;
+
+	BOOL InitPing( );
+	void StopPing( );
+	BOOL PingTest( );
+
+	BOOL IsSocketReadible(SOCKET socket, DWORD dwTimeout, BOOL& bReadible);
+	USHORT	GenerateIPChecksum(USHORT* pBuffer, int nSize);
+	void	FillIcmpData(LPICMP_HEADER pIcmp, int nData);
+	int	DecodeResponse(unsigned char* pBuf, int nBytes, sockaddr_in* from) ;
+};
+ 
   
