009856661240193903a25832d687663be0fc160f
lijin.unix <lijin.unix@gmail.com>
2009-11-18 14:12:25 +0000

    Modbus协议栈实现

---
+++ NXP-LPC/RT-Thread/UITC/Mbx/include/mbproto.h
@@ -0,0 +1,75 @@
+/****************************************Copyright (c)**************************************************
+**                                         
+**                                      
+**                                      
+**
+**                           
+**
+**--------------文件信息--------------------------------------------------------------------------------
+**文   件   名:  
+**创   建   人:  
+**最后修改日期: 
+**描        述:   
+**   File: $Id: mbproto.h,v 1.0 2009/11/15 15:27:16   $
+**--------------历史版本信息----------------------------------------------------------------------------
+** 创建人:  
+** 版  本:  
+** 日　期:  
+** 描　述: 
+**
+**--------------当前版本修订------------------------------------------------------------------------------
+** 修改人:
+** 日　期:
+** 描　述:  
+**
+**------------------------------------------------------------------------------------------------------
+********************************************************************************************************/
+
+
+#pragma  once
+
+
+/* ----------------------- Defines ------------------------------------------*/
+#define MB_ADDRESS_BROADCAST    ( 0 )   /*! Modbus broadcast address. */
+#define MB_ADDRESS_MIN          ( 1 )   /*! Smallest possible slave address. */
+#define MB_ADDRESS_MAX          ( 247 ) /*! Biggest possible slave address. */
+
+#define MB_FUNC_NONE                          (  0 )
+#define MB_FUNC_READ_COILS                    (  1 )
+#define MB_FUNC_READ_DISCRETE_INPUTS          (  2 )
+#define MB_FUNC_WRITE_SINGLE_COIL             (  5 )
+#define MB_FUNC_WRITE_MULTIPLE_COILS          ( 15 )
+#define MB_FUNC_READ_HOLDING_REGISTER         (  3 )
+#define MB_FUNC_READ_INPUT_REGISTER           (  4 )
+#define MB_FUNC_WRITE_REGISTER                (  6 )
+#define MB_FUNC_WRITE_MULTIPLE_REGISTERS      ( 16 )
+#define MB_FUNC_READWRITE_MULTIPLE_REGISTERS  ( 23 )
+#define MB_FUNC_DIAG_READ_EXCEPTION           (  7 )
+#define MB_FUNC_DIAG_DIAGNOSTIC               (  8 )
+#define MB_FUNC_DIAG_GET_COM_EVENT_CNT        ( 11 )
+#define MB_FUNC_DIAG_GET_COM_EVENT_LOG        ( 12 )
+#define MB_FUNC_OTHER_REPORT_SLAVEID          ( 17 )
+#define MB_FUNC_ERROR                         ( 128 )
+/* ----------------------- Type definitions ---------------------------------*/
+typedef enum
+{
+    MB_EX_NONE = 0x00,
+    MB_EX_ILLEGAL_FUNCTION = 0x01,
+    MB_EX_ILLEGAL_DATA_ADDRESS = 0x02,
+    MB_EX_ILLEGAL_DATA_VALUE = 0x03,
+    MB_EX_SLAVE_DEVICE_FAILURE = 0x04,
+    MB_EX_ACKNOWLEDGE = 0x05,
+    MB_EX_SLAVE_BUSY = 0x06,
+    MB_EX_MEMORY_PARITY_ERROR = 0x08,
+    MB_EX_GATEWAY_PATH_FAILED = 0x0A,
+    MB_EX_GATEWAY_TGT_FAILED = 0x0B
+} MBException;
+
+typedef    MBException( *fnMBFunctionHandler ) ( UCHAR * pucFrame, USHORT * pusLength );
+
+typedef struct
+{
+    UCHAR           ucFunctionCode;
+    fnMBFunctionHandler pfnHandler;
+} MBFunctionHandler;
+ 
